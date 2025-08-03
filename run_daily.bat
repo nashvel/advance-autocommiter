32121545577fa96bc721d1385908afae96d45eb2
nashvel <nash.backup21@gmail.com>
2025-08-04 06:53:42 +0800

    Auto-commit #1 - 2025-08-04 06:53:42

--- run_daily.bat
+++ run_daily.bat
@@ -1,4 +1,7 @@
 @echo off
+REM Change to script directory
 cd /d "c:\Users\user\OneDrive\Desktop\Projects\github-scipt\auto-commiter"
+REM Run the Python script
 python auto_committer.py --run-now
+REM Keep the terminal open
 pause
